631892fb9add97a8a1d6db576bfb821c93719bcc
alyab0uzaid <aabouza@siue.edu>
2024-09-19 11:15:09 -0500

    formating

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'; // Import getAuth stuff
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHdf8tVDVOtTazjvC0h1PyKwqNifWfqww",
-  authDomain: "trackmysci.firebaseapp.com",
-  projectId: "trackmysci",
-  storageBucket: "trackmysci.appspot.com",
-  messagingSenderId: "94634841161",
-  appId: "1:94634841161:web:ded58b1dc49db1f1dc1b98",
-  measurementId: "G-6JR45C2DBF"
+	apiKey: 'AIzaSyCHdf8tVDVOtTazjvC0h1PyKwqNifWfqww',
+	authDomain: 'trackmysci.firebaseapp.com',
+	projectId: 'trackmysci',
+	storageBucket: 'trackmysci.appspot.com',
+	messagingSenderId: '94634841161',
+	appId: '1:94634841161:web:ded58b1dc49db1f1dc1b98',
+	measurementId: 'G-6JR45C2DBF'
 };
 
 // Initialize Firebase
@@ -16,11 +16,13 @@ const app = initializeApp(firebaseConfig);
 
 // Only initialize Analytics if it's supported in the environment
 if (typeof window !== 'undefined') {
-  import('firebase/analytics').then(({ getAnalytics }) => {
-    getAnalytics(app);
-  }).catch((error) => {
-    console.log('Analytics not supported:', error);
-  });
+	import('firebase/analytics')
+		.then(({ getAnalytics }) => {
+			getAnalytics(app);
+		})
+		.catch((error) => {
+			console.log('Analytics not supported:', error);
+		});
 }
 
 // Initialize Firebase Authentication and get a reference to the service
@@ -29,9 +31,9 @@ export const provider = new GoogleAuthProvider();
 
 // Functions to handle login and logout
 export const loginWithGoogle = () => {
-  return signInWithPopup(auth, provider);
+	return signInWithPopup(auth, provider);
 };
 
 export const logout = () => {
-  return signOut(auth);
+	return signOut(auth);
 };
